ab600781900028bc2bc0725caaed6b4b24fff42b
Hamed Hasan <71956267+Hamed-Hasan@users.noreply.github.com>
2022-05-06 04:16:21 +0300

    conceptual-session-sagor-notes-taker

---
+++ conceptual-session-sagor-notes-taker/notes-taker-client/src/App.js
@@ -0,0 +1,144 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import InputForm from "./components/inputForm/InputForm";
+import NoteCard from "./components/noteCard/NoteCard";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [isReload, setIsReload] = useState(false);
+  useEffect(() => {
+
+   fetch("http://localhost:5000/notes")
+   .then(res=>res.json())
+   .then(data=>setNotes(data))
+
+    
+  }, [isReload]);
+
+  /*
+1. here there will be a function named handleSearch
+to handle search by query, and it will be passed as props to header
+
+  */
+const handleSearch = (event) => {
+  event.preventDefault();
+  const queryText = event.target.searchText.value;
+console.log(queryText);
+  if (queryText) {
+    fetch(`http://localhost:5000/notes?userName=${queryText}`)
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }
+};
+
+
+  
+/*2. here there will be a function named handleDelete
+to delete a note, and it will be passed as props to NoteCard that will be triggered using delete button.
+ */
+
+
+const handleDelete = (id) => {
+  console.log(id);
+
+  fetch(`http://localhost:5000/notes/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setIsReload(!isReload);
+    });
+
+};
+
+
+
+
+
+
+
+
+  /*
+3. there will be a function named handleUpdate
+    to update data, and it will be passed as props to NoteCard and 
+   later it will be passed to Update modal using props.
+ */
+
+//    const handleUpdate = (event, id) => {
+//     event.preventDefault();
+//     const name = event.target.name.value;
+//     const email = event.target.email.value;
+
+//     const updateUser = {name, email};
+
+//     // send data to server
+//     const url = `http://localhost:5000/notes/${id}`
+//     fetch(url, {
+//         method: 'PUT',
+//         headers: {
+//             "content-type": "application/json"
+//         },
+//         body: JSON.stringify(updateUser)
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//       setIsReload(!isReload);
+//         console.log(data)
+//         alert('user update successfully')
+//     })
+// }
+
+   
+
+
+
+  /*
+4.  there will be a function named handlePost
+to post data to backend, and it will be passed as props to InputFrom.
+ */
+const handlePost = (event) => {
+  event.preventDefault();
+  const userName = event.target.userName.value;
+  const textData = event.target.textData.value;
+
+  console.log({ userName, textData });
+
+  fetch("http://localhost:5000/notes", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+
+    body: JSON.stringify({ userName, textData }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setIsReload(!isReload);
+    });
+
+};
+  
+
+  return (
+    <div className="App">
+      <Header handleSearch={handleSearch} />
+      <InputForm handlePost={handlePost}/>
+      <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
+        {notes.map((note) => (
+          <NoteCard
+            note={note}
+            handleDelete={handleDelete}
+            setIsReload={setIsReload}
+            isReload={isReload}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ conceptual-session-sagor-notes-taker/notes-taker-client/src/components/updateModal/UpdateModal.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#root");
+
+
+//don't worry its just a package for modal. just go and explore https://www.npmjs.com/package/react-modal
+
+export default function UpdateModal({ id, setIsReload, isReload }) {
+
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const userName = event.target.userName.value;
+    const textData = event.target.textData.value;
+
+    const url = `http://localhost:5000/notes/${id}`
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ userName, textData })
+    })
+    .then(res => res.json())
+    .then(data => {
+      setIsReload(!isReload)
+      alert('update data')
+    })
+
+  };
+  return (
+    <div>
+      <button onClick={openModal} className="color-801336 btn-sm btn">
+        {" "}
+        Update
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal} className="btn btn-sm btn-warning">
+          close
+        </button>
+        <div>Please insert your text</div>
+        <div className=" p-3 color-4D4C7D">
+          <form className="container " onSubmit={handleUpdate}>
+            <div className="input-group mb-3 mt-5">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Your name"
+                aria-label="Username"
+                name="userName"
+              />
+            </div>
+
+            <div className="input-group">
+              <textarea
+                className="form-control"
+                aria-label="With textarea"
+                name="textData"
+              ></textarea>
+            </div>
+            <div className="mt-4">
+              <input type="submit" value="submit" className="btn btn-info" />
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
